3002a854c7af10ff46877ae7c4df1117188227af
tuan <tuanphung2308@gmail.com>
2021-01-26 03:22:17 +0700

    Initial commit

---
+++ app/src/main/java/com/material/components/tuan/ShoppingCartSimple.java
@@ -0,0 +1,59 @@
+package com.material.components.tuan;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.material.components.R;
+import com.material.components.utils.Tools;
+
+public class ShoppingCartSimple extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart_simple);
+        initToolbar();
+    }
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.grey_60), PorterDuff.Mode.SRC_ATOP);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Cart");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Tools.setSystemBarColor(this, R.color.grey_5);
+        Tools.setSystemBarLight(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Tools.changeMenuIconColor(menu, getResources().getColor(R.color.grey_60));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onCheckOut(View view) {
+        finish();
+        Intent intent = new Intent(this, ShoppingCheckoutStep.class);
+        intent.putExtra("fuck you", "fuck");
+        startActivity(intent);
+    }
+}
